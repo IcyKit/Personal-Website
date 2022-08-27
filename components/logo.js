0fffb896293ac9f457651a35208e03be26f92879
IcyKitMac <icykitdesign@gmail.com>
2022-08-27 17:12:27 +0300

    Updated: Logo transition updated

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Text, useColorModeValue } from '@chakra-ui/react';
 
 import styled from '@emotion/styled';
 
@@ -11,9 +11,10 @@ const LogoBox = styled.span`
   align-items: center;
   height: 30px;
   line-height: 20px;
-  padding: 10px;
+  padding: 15px;
   &:hover img {
-    transform: rotate(20deg);
+    transform: rotate(180deg);
+    transition: 0.4s all;
   }
 `
 
