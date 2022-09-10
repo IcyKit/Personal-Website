2d6bce23e7817122e704998dcef1dfd0f8b4b102
IcyKitMac <icykitdesign@gmail.com>
2022-09-10 07:31:50 +0300

    Updated: Font config updated

--- pages/stack.js
+++ pages/stack.js
@@ -6,58 +6,92 @@ import {
   List,
   ListItem,
   ListIcon,
-} from '@chakra-ui/react';
-import Section from '../components/section';
-import Layout from '../components/layouts/article';
-import { SiElectron, SiWebpack, SiBabel, SiEslint, SiNpm, SiPostgresql, SiSocketdotio, SiPostman, SiGraphql, SiReduxsaga, SiHtml5, SiCss3, SiJavascript, SiReact, SiRedux, SiNextdotjs, SiNodedotjs, SiBootstrap, SiMaterialui, SiChakraui, SiSass, SiTailwindcss, SiMarkdown, SiTypescript, SiSolidity, SiMongodb, SiExpress, SiGit, SiGithub, SiVisualstudiocode, SiVim, SiFigma, SiAdobephotoshop, SiBlender, } from 'react-icons/si';
+} from "@chakra-ui/react";
+import Section from "../components/section";
+import Layout from "../components/layouts/article";
+import {
+  SiElectron,
+  SiWebpack,
+  SiBabel,
+  SiEslint,
+  SiNpm,
+  SiPostgresql,
+  SiSocketdotio,
+  SiPostman,
+  SiGraphql,
+  SiReduxsaga,
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiRedux,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiBootstrap,
+  SiMaterialui,
+  SiChakraui,
+  SiSass,
+  SiTailwindcss,
+  SiMarkdown,
+  SiTypescript,
+  SiSolidity,
+  SiMongodb,
+  SiExpress,
+  SiGit,
+  SiGithub,
+  SiVisualstudiocode,
+  SiVim,
+  SiFigma,
+  SiAdobephotoshop,
+  SiBlender,
+} from "react-icons/si";
 
 const Stack = () => {
-
   const techStack = [
-    {title: 'HTML', icon: SiHtml5},
-    {title: 'CSS', icon: SiCss3},
-    {title: 'JavaScript', icon: SiJavascript},
-    {title: 'React', icon: SiReact},
-    {title: 'Redux', icon: SiRedux},
-    {title: 'Next.JS', icon: SiNextdotjs},
-    {title: 'Node.JS', icon: SiNodedotjs},
-    {title: 'Electron', icon: SiElectron},
-    {title: 'NPM and Yarn', icon: SiNpm},
-    {title: 'React Native', icon: SiReact},
-    {title: 'Webpack', icon: SiWebpack},
-    {title: 'Babel', icon: SiBabel},
-    {title: 'ESLint', icon: SiEslint},
-    {title: 'Bootstrap', icon: SiBootstrap},
-    {title: 'Material UI', icon: SiMaterialui},
-    {title: 'Chakra UI', icon: SiChakraui},
-    {title: 'Sass', icon: SiSass},
-    {title: 'Tailwind', icon: SiTailwindcss},
-    {title: 'Markdown', icon: SiMarkdown}
+    { title: "HTML", icon: SiHtml5 },
+    { title: "CSS", icon: SiCss3 },
+    { title: "JavaScript", icon: SiJavascript },
+    { title: "React", icon: SiReact },
+    { title: "Redux", icon: SiRedux },
+    { title: "Next.JS", icon: SiNextdotjs },
+    { title: "Node.JS", icon: SiNodedotjs },
+    { title: "Express", icon: SiExpress },
+    { title: "Electron", icon: SiElectron },
+    { title: "NPM and Yarn", icon: SiNpm },
+    { title: "React Native", icon: SiReact },
+    { title: "Webpack", icon: SiWebpack },
+    { title: "Babel", icon: SiBabel },
+    { title: "ESLint", icon: SiEslint },
+    { title: "Bootstrap", icon: SiBootstrap },
+    { title: "Material UI", icon: SiMaterialui },
+    { title: "Chakra UI", icon: SiChakraui },
+    { title: "Sass", icon: SiSass },
+    { title: "Tailwind", icon: SiTailwindcss },
+    { title: "Markdown", icon: SiMarkdown },
   ];
 
   const learningNow = [
-    {title: 'TypeScript', icon: SiTypescript},
-    {title: 'Solidity', icon: SiSolidity},
-    {title: 'PostgreSQL', icon: SiPostgresql}
+    { title: "TypeScript", icon: SiTypescript },
+    { title: "Solidity", icon: SiSolidity },
+    { title: "PostgreSQL", icon: SiPostgresql },
   ];
 
   const inPlans = [
-    {title: 'Express', icon: SiExpress},
-    {title: 'MongoDB', icon: SiMongodb},
-    {title: 'Socket.io', icon: SiSocketdotio},
-    {title: 'Postman', icon: SiPostman},
-    {title: 'GraphQL', icon: SiGraphql},
-    {title: 'ReduxSaga', icon: SiReduxsaga},
+    { title: "MongoDB", icon: SiMongodb },
+    { title: "Socket.io", icon: SiSocketdotio },
+    { title: "Postman", icon: SiPostman },
+    { title: "GraphQL", icon: SiGraphql },
+    { title: "ReduxSaga", icon: SiReduxsaga },
   ];
 
   const tools = [
-    {title: 'Git', icon: SiGit},
-    {title: 'GitHub', icon: SiGithub},
-    {title: 'VS Code', icon: SiVisualstudiocode},
-    {title: 'Vim', icon: SiVim},
-    {title: 'Figma', icon: SiFigma},
-    {title: 'Photoshop', icon: SiAdobephotoshop},
-    {title: 'Blender', icon: SiBlender},
+    { title: "Git", icon: SiGit },
+    { title: "GitHub", icon: SiGithub },
+    { title: "VS Code", icon: SiVisualstudiocode },
+    { title: "Vim", icon: SiVim },
+    { title: "Figma", icon: SiFigma },
+    { title: "Photoshop", icon: SiAdobephotoshop },
+    { title: "Blender", icon: SiBlender },
   ];
 
   return (
@@ -71,12 +105,13 @@ const Stack = () => {
         <Section delay={0.2}>
           <List spacing={4} fontSize={16}>
             <SimpleGrid spacing={5} columns={[1, 2, 3]}>
-              {techStack.map(item => {
+              {techStack.map((item) => {
                 return (
                   <ListItem>
-                    <ListIcon w={5} h={5} as={item.icon} />{item.title}
+                    <ListIcon w={5} h={5} as={item.icon} />
+                    {item.title}
                   </ListItem>
-                )
+                );
               })}
             </SimpleGrid>
           </List>
@@ -89,13 +124,14 @@ const Stack = () => {
         </Section>
         <Section delay={0.4}>
           <List spacing={4} fontSize={16}>
-          {learningNow.map(item => {
-                return (
-                  <ListItem>
-                    <ListIcon w={5} h={5} as={item.icon} />{item.title}
-                  </ListItem>
-                )
-              })}
+            {learningNow.map((item) => {
+              return (
+                <ListItem>
+                  <ListIcon w={5} h={5} as={item.icon} />
+                  {item.title}
+                </ListItem>
+              );
+            })}
           </List>
         </Section>
         <Section delay={0.6}>
@@ -106,13 +142,14 @@ const Stack = () => {
         </Section>
         <Section delay={0.6}>
           <List spacing={4} fontSize={16}>
-          {inPlans.map(item => {
-                return (
-                  <ListItem>
-                    <ListIcon w={5} h={5} as={item.icon} />{item.title}
-                  </ListItem>
-                )
-              })}
+            {inPlans.map((item) => {
+              return (
+                <ListItem>
+                  <ListIcon w={5} h={5} as={item.icon} />
+                  {item.title}
+                </ListItem>
+              );
+            })}
           </List>
         </Section>
         <Section delay={0.8}>
@@ -123,18 +160,19 @@ const Stack = () => {
         </Section>
         <Section delay={0.8}>
           <List spacing={4} fontSize={16}>
-          {tools.map(item => {
-                return (
-                  <ListItem>
-                    <ListIcon w={5} h={5} as={item.icon} />{item.title}
-                  </ListItem>
-                )
-              })}
+            {tools.map((item) => {
+              return (
+                <ListItem>
+                  <ListIcon w={5} h={5} as={item.icon} />
+                  {item.title}
+                </ListItem>
+              );
+            })}
           </List>
         </Section>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
 export default Stack;
